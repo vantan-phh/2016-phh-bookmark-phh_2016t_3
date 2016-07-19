9b491eabacf95e201405bb93153da5c83ca3b081
coco <koko_16N1300059@nnn.ed.jp>
2016-07-19 16:57:32 +0900

    Enable to save bookmark data and display it

--- social_bookmark/routes/myBookmarkEdit.js
+++ social_bookmark/routes/myBookmarkEdit.js
@@ -13,7 +13,14 @@ router.get('/',function(req,res){
   });
 });
 router.post('/',function(req,res){
-
+  var userId = req.session.user_id;
+  var url = req.session.url;
+  var title = req.body.title;
+  var description = req.body.description;
+  var query = 'INSERT INTO `bookmarks` (`user_id`,`title`,`url`,`description`) VALUES(?, ?, ?, ?)';
+  connection.query(query,[userId,title,url,description],function(err,result){
+    res.redirect('/PHH_Bookmark/myPage');
+  });
 });
 
 module.exports = router;
