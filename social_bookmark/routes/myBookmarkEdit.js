77f8be06302c6f72aacc5a072e8e0333d1c54ec3
coco <koko_16N1300059@nnn.ed.jp>
2016-07-19 22:16:54 +0900

    Fix the bug that is caused by too long title or description.

--- social_bookmark/routes/myBookmarkEdit.js
+++ social_bookmark/routes/myBookmarkEdit.js
@@ -17,6 +17,12 @@ router.post('/',function(req,res){
   var url = req.session.url;
   var title = req.body.title;
   var description = req.body.description;
+  if(title.length > 32){
+    alert('タイトルが長すぎます。');
+  }
+  if(description.length > 128){
+    alert('説明が長すぎます。');
+  }
   var query = 'INSERT INTO `bookmarks` (`user_id`,`title`,`url`,`description`) VALUES(?, ?, ?, ?)';
   connection.query(query,[userId,title,url,description],function(err,result){
     res.redirect('/PHH_Bookmark/myPage');
