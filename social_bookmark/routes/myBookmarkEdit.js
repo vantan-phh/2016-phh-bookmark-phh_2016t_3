384975aea547f309222f83a70ce07dbf64f2531b
coco <koko_16N1300059@nnn.ed.jp>
2016-07-23 12:07:59 +0900

    Fix the bug of too long title and description

--- social_bookmark/routes/myBookmarkEdit.js
+++ social_bookmark/routes/myBookmarkEdit.js
@@ -17,16 +17,28 @@ router.post('/',function(req,res){
   var url = req.session.url;
   var title = req.body.title;
   var description = req.body.description;
-  if(title.length > 32){
-    alert('タイトルが長すぎます。');
+  if(title.length > 32 && description.length > 128){
+    res.render('myBookmarkEdit.ejs',{
+      tooLongTitle : 'タイトルが長すぎます。',
+      tooLongDescription : '説明が長すぎます。',
+      title : title
+    });
+  }else if(title.length > 32){
+    res.render('myBookmarkEdit.ejs',{
+      tooLongTitle : 'タイトルが長すぎます。',
+      title : title
+    });
+  }else if(description.length > 128){
+    res.render('myBookmarkEdit.ejs',{
+      tooLongDescription : '説明が長すぎます。',
+      title : title
+    });
+  }else{
+    var query = 'INSERT INTO `bookmarks` (`user_id`,`title`,`url`,`description`) VALUES(?, ?, ?, ?)';
+    connection.query(query,[userId,title,url,description],function(err,result){
+      res.redirect('/PHH_Bookmark/myPage');
+    });
   }
-  if(description.length > 128){
-    alert('説明が長すぎます。');
-  }
-  var query = 'INSERT INTO `bookmarks` (`user_id`,`title`,`url`,`description`) VALUES(?, ?, ?, ?)';
-  connection.query(query,[userId,title,url,description],function(err,result){
-    res.redirect('/PHH_Bookmark/myPage');
-  });
 });
 
 module.exports = router;
