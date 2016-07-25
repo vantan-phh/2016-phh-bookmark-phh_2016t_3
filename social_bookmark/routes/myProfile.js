cadf53a8680bda72e5615b74944a0db2ae96bd45
coco <koko_16N1300059@nnn.ed.jp>
2016-07-25 17:01:56 +0900

    Implement upload images on profile

--- social_bookmark/routes/myProfile.js
+++ social_bookmark/routes/myProfile.js
@@ -1,28 +1,36 @@
 var express = require('express');
 var router = express.Router();
 var connection = require('../mysqlConnection');
+var multer = require('multer');
+var upload = multer({dest:'./PHH_Bookmark/view/img/uploads/'});
+var cloudinary = require('cloudinary');
+cloudinary.config({
+  cloud_name: 'dy4f7hul5',
+  api_key: '925664739655858',
+  api_select: 'sbP8YsyWrhbf-vyZDsq4-6Izd_8'
+});
 
 router.get('/',function(req,res){
   var userId = req.session.user_id;
-  var query = 'SELECT `name`,`nick_name`,`img_dat`,`introduction` FROM `users` WHERE `user_id` = ?';
+  var query = 'SELECT `name`,`nick_name`,`image_path`,`introduction` FROM `users` WHERE `user_id` = ?';
   connection.query(query,[userId],function(err,result){
     var nickName = result[0].nick_name;
-    var thumbnail = result[0].img_dat;
+    var thumbnailPath = result[0].image_path;
     var introduction = result[0].introduction;
     var userName = result[0].name;
-    console.log(thumbnail);
+    console.log(thumbnailPath);
     if(nickName === null){
       nickName = userName;
     }
-    if(thumbnail === null){
-      thumbnail = 'img/favicon_Prototype.ico';
+    if(thumbnailPath === null){
+      thumbnailPath = 'img/favicon_Prototype.ico';
     }
     if(introduction === null){
       introduction = '自己紹介';
     }
     res.render('myProfile.ejs',{
       nickName : nickName,
-      thumbnail : thumbnail,
+      thumbnailPath : thumbnailPath,
       introduction : introduction
     });
   });
--- social_bookmark/routes/myProfileEdit.js
+++ social_bookmark/routes/myProfileEdit.js
@@ -7,7 +7,7 @@ var cloudinary = require('cloudinary');
 cloudinary.config({
   cloud_name: 'dy4f7hul5',
   api_key: '925664739655858',
-  api_select: 'sbP8YsyWrhbf-vyZDsq4-6Izd_8'
+  api_secret: 'sbP8YsyWrhbf-vyZDsq4-6Izd_8'
 });
 
 router.get('/',function(req,res){
@@ -16,16 +16,16 @@ router.get('/',function(req,res){
   res.render('myProfileEdit.ejs');
 });
 
-router.post('/',upload.single('img_file'),function(req,res){
+router.post('/',upload.single('image_file'),function(req,res){
   var nickName = req.body.nick_name;
   var path = req.file.path;
   var introduction = req.body.introduction;
   var userId = req.session.user_id;
   cloudinary.uploader.upload(path, function(result){
     var imagePath = result.url;
-    var query = 'INSERT INTO `users` (`nick_name`,`image_path`,`introduction`) VALUES ( ?, ?, ?)';
-    connection.query(query,[nickName,imagePath,introduction],function(err,result){
-      res.redirect('PHH_Bookmark/myProfile');
+    var query = 'UPDATE `users` SET `nick_name` = ?,`image_path` = ?,`introduction` = ? WHERE `user_id` = ?';
+    connection.query(query,[nickName,imagePath,introduction,userId],function(err,result){
+      res.redirect('/PHH_Bookmark/myProfile');
     });
   });
 });
