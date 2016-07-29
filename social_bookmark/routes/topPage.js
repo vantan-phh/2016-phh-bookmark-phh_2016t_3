ae333d6c46f694fda4412135d3690f398bbd0733
coco <koko_16N1300059@nnn.ed.jp>
2016-07-29 18:14:09 +0900

    Delete a in topPage.js

--- social_bookmark/routes/topPage.js
+++ social_bookmark/routes/topPage.js
@@ -3,11 +3,11 @@ var router = express.Router();
 var connection = require('../mysqlConnection');
 
 router.get('/',function(req,res){
-  var a = 1;
+  var userId = req.session.user_id;
   var query = 'SELECT * FROM `bookmarks` WHERE `user_id` = ?';
-  connection.query(query,[a],function(err,rows){
+  connection.query(query,[userId],function(err,result){
     res.render('topPage.ejs',{
-      list : rows
+      list : result
     });
   });
 });
@@ -16,7 +16,7 @@ router.post('/submit',function(req,res){
   var newBookmarkTitle = req.body.title;
   var newBookmarkDescription = req.body.description;
   var query = 'INSERT INTO `user_bookmarks` (`title`,`url`,`description`) VALUES(?, ? ,?)';
-  connection.query(query,[newBookmarkTitle,newBookmarkUrl,newBookmarkDescription],function(err,rows){
+  connection.query(query,[newBookmarkTitle,newBookmarkUrl,newBookmarkDescription],function(err,result){
     res.redirect('/topPage');
   });
 });
