572722569c43882185cea285218bb9b202f2d082
coco <koko_16N1300059@nnn.ed.jp>
2016-07-29 16:20:50 +0900

    Not to create organization when the same name organization exists

--- social_bookmark/routes/createOrganization.js
+++ social_bookmark/routes/createOrganization.js
@@ -133,62 +133,75 @@ router.post('/create',upload.single('image_file'),function(req,res){
   var orgName = req.body.orgName;
   var orgIntroduction = req.body.orgIntroduction;
   var myId = req.session.user_id;
+  var orgNameExists = 'SELECT `name` FROM `organizations` WHERE `name` = ?';
   var selectMyUserName = 'SELECT `name` FROM `users` WHERE `user_id` = ?';
-  connection.query(selectMyUserName,[myId],function(err,result){
-    var myUserName = result[0].name;
-    selectedUserNames.push(myUserName);
-    if(req.file){
-      var path = req.file.path;
-      cloudinary.uploader.upload(path,function(result){
-        var orgThumbnail = result.url;
-        var createOrgQuery = 'INSERT INTO `organizations` (`name`,`image_path`,`introduction`) VALUES(?, ?, ?)';
-        connection.query(createOrgQuery,[orgName,orgThumbnail,orgIntroduction],function(err,result){
-          var selectOrgId = 'SELECT `id` FROM `organizations` WHERE `name` = ?';
-          connection.query(selectOrgId,[orgName],function(err,result){
-            var orgId = result[0].id;
-            var selectUserId = 'SELECT `user_id` FROM `users` WHERE `name` = ?';
-            for(var i = 0; i < selectedUserNames.length; i++){
-              connection.query(selectUserId,[selectedUserNames[i]],function(err,result){
-                var userId = result[0].user_id;
-                var intoMembershipsQuery = 'INSERT INTO `organization_memberships` (`user_id`,`org_id`,`is_admin`) VALUES(?, ?, ?)';
-                if(userId !== req.session.user_id){
-                  connection.query(intoMembershipsQuery,[userId,orgId,false],function(err,result){
-                  });
-                } else if(userId === req.session.user_id){
-                  connection.query(intoMembershipsQuery,[userId,orgId,true],function(err,result){
-                    res.redirect('/PHH_Bookmark/organizationPage');
+  connection.query(orgNameExists,[orgName],function(err,result){
+    if(result.length < 1){
+      connection.query(selectMyUserName,[myId],function(err,result){
+        var myUserName = result[0].name;
+        selectedUserNames.push(myUserName);
+        if(req.file){
+          var path = req.file.path;
+          cloudinary.uploader.upload(path,function(result){
+            var orgThumbnail = result.url;
+            var createOrgQuery = 'INSERT INTO `organizations` (`name`,`image_path`,`introduction`) VALUES(?, ?, ?)';
+            connection.query(createOrgQuery,[orgName,orgThumbnail,orgIntroduction],function(err,result){
+              var selectOrgId = 'SELECT `id` FROM `organizations` WHERE `name` = ?';
+              connection.query(selectOrgId,[orgName],function(err,result){
+                var orgId = result[0].id;
+                var selectUserId = 'SELECT `user_id` FROM `users` WHERE `name` = ?';
+                for(var i = 0; i < selectedUserNames.length; i++){
+                  connection.query(selectUserId,[selectedUserNames[i]],function(err,result){
+                    var userId = result[0].user_id;
+                    var intoMembershipsQuery = 'INSERT INTO `organization_memberships` (`user_id`,`org_id`,`is_admin`) VALUES(?, ?, ?)';
+                    if(userId !== req.session.user_id){
+                      connection.query(intoMembershipsQuery,[userId,orgId,false],function(err,result){
+                      });
+                    } else if(userId === req.session.user_id){
+                      connection.query(intoMembershipsQuery,[userId,orgId,true],function(err,result){
+                        res.redirect('/PHH_Bookmark/organizationPage');
+                      });
+                    }
                   });
                 }
               });
-            }
+            });
           });
-        });
-      });
-    }else{ //when file data wasn't submitted
-      var orgThumbnail = 'http://res.cloudinary.com/dy4f7hul5/image/upload/v1469755753/y7mekcjoe7zptq7zl3ti.png';
-      var createOrgQuery = 'INSERT INTO `organizations` (`name`,`image_path`,`introduction`) VALUES(?, ?, ?)';
-      connection.query(createOrgQuery,[orgName,orgThumbnail,orgIntroduction],function(err,result){
-        var selectOrgId = 'SELECT `id` FROM `organizations` WHERE `name` = ?';
-        connection.query(selectOrgId,[orgName],function(err,result){
-          var orgId = result[0].id;
-          var selectUserId = 'SELECT `user_id` FROM `users` WHERE `name` = ?';
-          for(var i = 0; i < selectedUserNames.length; i++){
-            connection.query(selectUserId,[selectedUserNames[i]],function(err,result){
-              var userId = result[0].user_id;
-              var intoMembershipsQuery = 'INSERT INTO `organization_memberships` (`user_id`,`org_id`,`is_admin`) VALUES(?, ?, ?)';
-              if(userId !== req.session.user_id){
-                connection.query(intoMembershipsQuery,[userId,orgId,false],function(err,result){
-                });
-              } else if(userId === req.session.user_id){
-                connection.query(intoMembershipsQuery,[userId,orgId,true],function(err,result){
-                  res.redirect('/PHH_Bookmark/organizationPage');
+        }else{ //when file data wasn't submitted
+          var orgThumbnail = 'http://res.cloudinary.com/dy4f7hul5/image/upload/v1469755753/y7mekcjoe7zptq7zl3ti.png';
+          var createOrgQuery = 'INSERT INTO `organizations` (`name`,`image_path`,`introduction`) VALUES(?, ?, ?)';
+          connection.query(createOrgQuery,[orgName,orgThumbnail,orgIntroduction],function(err,result){
+            var selectOrgId = 'SELECT `id` FROM `organizations` WHERE `name` = ?';
+            connection.query(selectOrgId,[orgName],function(err,result){
+              var orgId = result[0].id;
+              var selectUserId = 'SELECT `user_id` FROM `users` WHERE `name` = ?';
+              for(var i = 0; i < selectedUserNames.length; i++){
+                connection.query(selectUserId,[selectedUserNames[i]],function(err,result){
+                  var userId = result[0].user_id;
+                  var intoMembershipsQuery = 'INSERT INTO `organization_memberships` (`user_id`,`org_id`,`is_admin`) VALUES(?, ?, ?)';
+                  if(userId !== req.session.user_id){
+                    connection.query(intoMembershipsQuery,[userId,orgId,false],function(err,result){
+                    });
+                  } else if(userId === req.session.user_id){
+                    connection.query(intoMembershipsQuery,[userId,orgId,true],function(err,result){
+                      res.redirect('/PHH_Bookmark/organizationPage');
+                    });
+                  }
                 });
               }
             });
-          }
-        });
+          });
+        };
       });
-    };
+    } else { // when submitted orgName is already exist in DB
+      res.render('createOrganization',{
+        orgName : orgName,
+        orgIntroduction : orgIntroduction,
+        selectedUserNames : selectedUserNames,
+        selectedUserNickNames : selectedUserNickNames,
+        orgNameExists : '同じ名前の組織が存在しています。'
+      });
+    }
   });
 });
 
