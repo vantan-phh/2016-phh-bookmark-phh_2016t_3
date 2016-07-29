2ca653222d221aaca5d65df3f6504e1a3c3b5d48
Yuya <yuuya_16n1100568@nnn.ed.jp>
2016-07-29 19:52:59 +0900

    Upgread header

--- social_bookmark/routes/setUser.js
+++ social_bookmark/routes/setUser.js
@@ -3,10 +3,11 @@ var connection = require('../mysqlConnection');
 module.exports = function(req,res,next){
   var userId = req.session.user_id;
   if(userId){
-    var query = 'SELECT `name` FROM `users` WHERE `user_id` = ?';
+    var query = 'SELECT `*` FROM `users` WHERE `user_id` = ?';
     connection.query(query,[userId], function(err,result){
       if(!err){
-        res.locals.user = result.length? result[0]:false;
+        res.locals.userName = result.length ? result[0].name : false;
+        res.locals.nickName = result.length ? result[0].nick_name : false;
       }
     });
   }else{
