d085531600bb884c561ec898860463e16b95a8e2
杉原 <shinta_16n1200325@nnn.ed.jp>
2016-08-08 18:18:17 +0900

    Enable to reject if posted nick name is only space on myProfileEdit

--- social_bookmark/routes/myProfileEdit.js
+++ social_bookmark/routes/myProfileEdit.js
@@ -30,36 +30,52 @@ router.post('/',upload.single('image_file'),function(req,res){
   var nickName = req.body.nick_name;
   var introduction = req.body.introduction;
   var checkInjection = /[%;+-]+/g;
-  if(!checkInjection.test(introduction)){
-    if(!checkInjection.test(nickName)){
-      if(nickName.length <= 32){
-        if(introduction.length <= 128){
-          if(req.file !== undefined){
-            var path = req.file.path;
-            if(nickName === ''){
-              var query = 'SELECT `name` FROM `users` WHERE `user_id` = ?';
-              connection.query(query,[userId],function(err,result){
-                nickName = result[0].name;
+  var checkSpace = /[\S]+/g;
+  if(checkSpace.test(nickName)){
+    if(!checkInjection.test(introduction)){
+      if(!checkInjection.test(nickName)){
+        if(nickName.length <= 32){
+          if(introduction.length <= 128){
+            if(req.file !== undefined){
+              var path = req.file.path;
+              if(nickName === ''){
+                var query = 'SELECT `name` FROM `users` WHERE `user_id` = ?';
+                connection.query(query,[userId],function(err,result){
+                  nickName = result[0].name;
+                });
+              }
+              cloudinary.uploader.upload(path, function(result){
+                var imagePath = result.url;
+                console.log(imagePath);
+                var query = 'UPDATE `users` SET `nick_name` = ?,`image_path` = ?,`introduction` = ? WHERE `user_id` = ?';
+                connection.query(query,[nickName,imagePath,introduction,userId],function(err,result){
+                  res.redirect('/PHH_Bookmark/myProfile');
+                });
               });
-            }
-            cloudinary.uploader.upload(path, function(result){
-              var imagePath = result.url;
-              console.log(imagePath);
-              var query = 'UPDATE `users` SET `nick_name` = ?,`image_path` = ?,`introduction` = ? WHERE `user_id` = ?';
-              connection.query(query,[nickName,imagePath,introduction,userId],function(err,result){
+            }else{
+              if(nickName === ''){
+                var query = 'SELECT `name` FROM `users` WHERE `user_id` = ?';
+                connection.query(query,[userId],function(err,result){
+                  nickName = result[0].name;
+                });
+              }
+              var query = 'UPDATE `users` SET `nick_name` = ?, `introduction` = ? WHERE `user_id` = ?';
+              connection.query(query,[nickName,introduction,userId],function(err,result){
                 res.redirect('/PHH_Bookmark/myProfile');
               });
-            });
-          }else{
-            if(nickName === ''){
-              var query = 'SELECT `name` FROM `users` WHERE `user_id` = ?';
-              connection.query(query,[userId],function(err,result){
-                nickName = result[0].name;
-              });
             }
-            var query = 'UPDATE `users` SET `nick_name` = ?, `introduction` = ? WHERE `user_id` = ?';
-            connection.query(query,[nickName,introduction,userId],function(err,result){
-              res.redirect('/PHH_Bookmark/myProfile');
+          }else{
+            var userId = req.session.user_id;
+            var query = 'SELECT * FROM `users` WHERE `user_id` = ?';
+            connection.query(query,[userId],function(err,result){
+              var nickName = result[0].nick_name;
+              var imagePath = result[0].image_path;
+              res.render('myProfileEdit.ejs',{
+                nickName : nickName,
+                imagePath : imagePath,
+                introduction : introduction,
+                introductionNotice : '自己紹介文は128文字以下です'
+              });
             });
           }
         }else{
@@ -72,9 +88,9 @@ router.post('/',upload.single('image_file'),function(req,res){
               nickName : nickName,
               imagePath : imagePath,
               introduction : introduction,
-              introductionNotice : '自己紹介文は128文字以下です'
+              nickNameNotice : 'ニックネームは32文字以下です'
             });
-          })
+          });
         }
       }else{
         var userId = req.session.user_id;
@@ -86,7 +102,7 @@ router.post('/',upload.single('image_file'),function(req,res){
             nickName : nickName,
             imagePath : imagePath,
             introduction : introduction,
-            nickNameNotice : 'ニックネームは32文字以下です'
+            nickNameNotice : 'セキュリティ上の観点からニックネームに「+, -, %, ;」は使えません'
           });
         });
       }
@@ -100,23 +116,23 @@ router.post('/',upload.single('image_file'),function(req,res){
           nickName : nickName,
           imagePath : imagePath,
           introduction : introduction,
-          nickNameNotice : 'セキュリティ上の観点からニックネームに「+, -, %, ;」は使えません'
+          introductionNotice : 'セキュリティ上の観点から自己紹介文に「+, -, %, ;」は使えません'
         });
-      })
+      });
     }
   }else{
     var userId = req.session.user_id;
     var query = 'SELECT * FROM `users` WHERE `user_id` = ?';
     connection.query(query,[userId],function(err,result){
-      var nickName = result[0].nick_name;
+      var nickName = undefined;
       var imagePath = result[0].image_path;
       res.render('myProfileEdit.ejs',{
         nickName : nickName,
         imagePath : imagePath,
         introduction : introduction,
-        introductionNotice : 'セキュリティ上の観点から自己紹介文に「+, -, %, ;」は使えません'
+        nickNameNotice : 'ニックネームを入力してください'
       });
-    })
+    });
   }
 });
 
