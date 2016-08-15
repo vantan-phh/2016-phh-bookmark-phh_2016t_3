9c280422d2c6986171692090d6aad84cf6eae3e9
coco <koko_16N1300059@nnn.ed.jp>
2016-08-15 17:32:15 +0900

    Clean code in myPage.js

--- social_bookmark/routes/myPage.js
+++ social_bookmark/routes/myPage.js
@@ -1,140 +1,136 @@
 var express = require('express');
+
 var router = express.Router();
 var connection = require('../mysqlConnection');
 var client = require('cheerio-httpcli');
+
 var bookmarkData;
 
-router.get('/',function(req,res){
+router.get('/',(req,res) => {
   var userId = req.session.user_id;
   var query = 'SELECT * FROM `bookmarks` WHERE `user_id` = ?';
-  connection.query(query,[userId],function(err,result){
-    bookmarkData = result;
+  connection.query(query,[userId]).then((result) => {
+    bookmarkData = result[0];
     res.render('myPage.ejs',{
-      bookmarkData : bookmarkData
+      bookmarkData,
     });
   });
 });
 
-router.post('/',function(req,res){
+router.post('/',(req,res) => {
   var userId = req.session.user_id;
   var url = req.body.url;
   var title = req.body.title;
   var description = req.body.description;
   var checkUrl = /^(https?)(:\/\/[-_.!~*\'()a-zA-Z0-9;\/?:\@&=+\$,%#]+)$/;
   var checkInjection = /[%;+-]+/g;
+  var query = 'SELECT * FROM `bookmarks` WHERE `user_id` = ?';
   if(checkUrl.test(url)){
     if(!checkInjection.test(title)){
       if(!checkInjection.test(description)){
         if(title.length <= 32){
           if(description.length <= 128){
             var createBookmark = 'INSERT INTO `bookmarks` (`user_id`,`title`,`url`,`description`) VALUES (?, ?, ?, ?)';
-            connection.query(createBookmark,[userId,title,url,description]);
-            res.redirect('/PHH_Bookmark/myPage');
+            connection.query(createBookmark,[userId,title,url,description]).then(() => {
+              res.redirect('/PHH_Bookmark/myPage');
+            });
           }else{
-            var userId = req.session.user_id;
-            var query = 'SELECT * FROM `bookmarks` WHERE `user_id` = ?';
-            connection.query(query,[userId],function(err,result){
-              bookmarkData = result;
+            connection.query(query,[userId]).then((result) => {
+              bookmarkData = result[0];
               res.render('myPage.ejs',{
-                bookmarkData : bookmarkData,
-                url : url,
-                title : title,
-                description : description,
-                descriptionNotice : '説明文は128文字以内です'
+                bookmarkData,
+                url,
+                title,
+                description,
+                descriptionNotice : '説明文は128文字以内です',
               });
             });
           }
         }else{
-          var userId = req.session.user_id;
-          var query = 'SELECT * FROM `bookmarks` WHERE `user_id` = ?';
-          connection.query(query,[userId],function(err,result){
-            bookmarkData = result;
+          connection.query(query,[userId]).then((result) => {
+            bookmarkData = result[0];
             res.render('myPage.ejs',{
-              bookmarkData : bookmarkData,
-              url : url,
-              title : title,
-              description : description,
-              titleNotice : 'タイトルは32文字以内です'
+              bookmarkData,
+              url,
+              title,
+              description,
+              titleNotice : 'タイトルは32文字以内です',
             });
           });
         }
       }else{
-        var userId = req.session.user_id;
-        var query = 'SELECT * FROM `bookmarks` WHERE `user_id` = ?';
-        connection.query(query,[userId],function(err,result){
-          bookmarkData = result;
+        connection.query(query,[userId]).then((result) => {
+          bookmarkData = result[0];
           res.render('myPage.ejs',{
-            bookmarkData : bookmarkData,
-            url : url,
-            title : title,
-            description : description,
-            descriptionNotice : 'セキュリティ上の観点から説明文に「+, -, %, ;」は使えません'
+            bookmarkData,
+            url,
+            title,
+            description,
+            descriptionNotice : 'セキュリティ上の観点から説明文に「+, -, %, ;」は使えません',
           });
         });
       }
     }else{
-      var userId = req.session.user_id;
-      var query = 'SELECT * FROM `bookmarks` WHERE `user_id` = ?';
-      connection.query(query,[userId],function(err,result){
-        bookmarkData = result;
+      connection.query(query,[userId]).then((result) => {
+        bookmarkData = result[0];
         res.render('myPage.ejs',{
-          bookmarkData : bookmarkData,
-          url : url,
-          title : title,
-          description : description,
-          titleNotice : 'セキュリティ上の観点からタイトルに「+, -, %, ;」は使えません'
+          bookmarkData,
+          url,
+          title,
+          description,
+          titleNotice : 'セキュリティ上の観点からタイトルに「+, -, %, ;」は使えません',
         });
       });
     }
   }else{
-    var userId = req.session.user_id;
-    var query = 'SELECT * FROM `bookmarks` WHERE `user_id` = ?';
-    connection.query(query,[userId],function(err,result){
-      bookmarkData = result;
+    connection.query(query,[userId]).then((result) => {
+      bookmarkData = result[0];
       res.render('myPage.ejs',{
-        bookmarkData : bookmarkData,
-        urlNotice : 'http://もしくはhttp://から始まる正しいURLを入力してください'
+        bookmarkData,
+        urlNotice : 'http://もしくはhttp://から始まる正しいURLを入力してください',
       });
     });
   }
 });
 
-router.post('/submitUrl',function(req,res){
+router.post('/submitUrl',(req,res) => {
   var url = req.body.result;
   var userId = req.session.user_id;
   var checkUrl = /^(https?)(:\/\/[-_.!~*\'()a-zA-Z0-9;\/?:\@&=+\$,%#]+)$/;
   if(checkUrl.test(url)){
-    var selectBookmarkData = 'SELECT * FROM `bookmarks` WHERE `user_id` = ?';
-    connection.query(selectBookmarkData,[userId],function(err,result){
-      var description = result[0].description;
-      client.fetch(url).then(function(result){
-        res.render('myPage.ejs',{
-          bookmarkData : bookmarkData,
-          title : result.$('title').text(),
-          url : url
-        });
+    client.fetch(url).then((result) => {
+      res.render('myPage.ejs',{
+        bookmarkData,
+        title : result.$('title').text(),
+        url,
       });
     });
   }else{
-    var userId = req.session.user_id;
     var query = 'SELECT * FROM `bookmarks` WHERE `user_id` = ?';
-    connection.query(query,[userId],function(err,result){
-      bookmarkData = result;
+    connection.query(query,[userId]).then((result) => {
+      bookmarkData = result[0];
       res.render('myPage.ejs',{
-        bookmarkData : bookmarkData,
-        urlNotice : 'http://もしくはhttp://から始まる正しいURLを入力してください'
+        bookmarkData,
+        urlNotice : 'http://もしくはhttp://から始まる正しいURLを入力してください',
       });
     });
   }
 });
 
-router.post('/delete',function(req,res){
+router.post('/delete',(req,res) => {
   var ids = req.body;
-  for(var x in ids){
-    var query = 'DELETE FROM `bookmarks` WHERE `bookmark_id` = ?'
-    connection.query(query,[x]);
-  }
-  res.redirect('/PHH_Bookmark/myPage');
+  (() => {
+    var promise = new Promise((resolve) => {
+      for(var x in ids){
+        var query = 'DELETE FROM `bookmarks` WHERE `bookmark_id` = ?';
+        connection.query(query,[x]);
+      }
+      resolve();
+    });
+    return promise;
+  })().then(() => {
+    res.redirect('/PHH_Bookmark/myPage');
+  });
 });
 
 module.exports = router;
