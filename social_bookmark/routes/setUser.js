5c3d1f4826e6b8734c1a0a507ee0856730e0838f
coco <koko_16N1300059@nnn.ed.jp>
2016-08-17 13:48:34 +0900

    Clean code in setUser.js and logout.js

--- social_bookmark/routes/setUser.js
+++ social_bookmark/routes/setUser.js
@@ -1,17 +1,15 @@
 var connection = require('../mysqlConnection');
 
-module.exports = function(req,res,next){
+module.exports = (req, res, next) => {
   var userId = req.session.user_id;
   if(userId){
     var query = 'SELECT `*` FROM `users` WHERE `user_id` = ?';
-    connection.query(query,[userId], function(err,result){
-      if(!err){
-        res.locals.userName = result.length ? result[0].name : false;
-        res.locals.nickName = result.length ? result[0].nick_name : false;
-      }
+    connection.query(query, [userId]).then((result) => {
+      res.locals.userName = result.length ? result[0][0].name : false;
+      res.locals.nickName = result.length ? result[0][0].nick_name : false;
     });
   }else{
     res.redirect('/PHH_Bookmark/login');
   }
   next();
-}
+};
