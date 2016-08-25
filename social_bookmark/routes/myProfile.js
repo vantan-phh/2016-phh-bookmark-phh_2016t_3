b35b2ab16403cf89ce15f84352900e43f5f65a7c
coco <koko_16N1300059@nnn.ed.jp>
2016-08-25 19:08:38 +0900

    Implement function of displaying other users' profile

--- social_bookmark/routes/myProfile.js
+++ social_bookmark/routes/myProfile.js
@@ -27,6 +27,42 @@ router.get('/', (req, res) => {
     });
     return promise;
   })().then((values) => {
+    var promise = new Promise((resolve) => {
+      var selectBelongOrg = 'SELECT `org_id` FROM `organization_memberships` WHERE `user_id` = ?';
+      connection.query(selectBelongOrg, [userId]).then((result) => {
+        values.selectedBelongOrgIds = result[0];
+        resolve(values);
+      });
+    });
+    return promise;
+  }).then((values) => {
+    var promise = new Promise((resolve) => {
+      var belongOrgIdsForQuery = '';
+      if(values.selectedBelongOrgIds.length > 0){
+        values.selectedBelongOrgIds.forEach((currentValue, index, array) => {
+          if(index + 1 === array.length){
+            belongOrgIdsForQuery += currentValue.org_id;
+            values.belongOrgIdsForQuery = belongOrgIdsForQuery;
+            resolve(values);
+          }else{
+            belongOrgIdsForQuery += currentValue.org_id + ' OR `id` = ';
+          }
+        });
+      }else{
+        resolve(values);
+      }
+    });
+    return promise;
+  }).then((values) => {
+    var promise = new Promise((resolve) => {
+      var selectOrgData = 'SELECT * FROM `organizations` WHERE `id` = ' + values.belongOrgIdsForQuery;
+      connection.query(selectOrgData).then((result) => {
+        values.orgData = result[0];
+        resolve(values);
+      });
+    });
+    return promise;
+  }).then((values) => {
     var selectRecentBookmarks = 'SELECT * FROM `bookmarks` WHERE `user_id` = ? ORDER BY `bookmark_id` DESC LIMIT 5';
     connection.query(selectRecentBookmarks, [userId]).then((result) => {
       res.render('userProfile.ejs', {
@@ -35,6 +71,7 @@ router.get('/', (req, res) => {
         thumbnailPath : values.thumbnailPath,
         introduction : values.introduction,
         recentBookmarks : result[0],
+        orgData : values.orgData,
       });
     });
   });
--- social_bookmark/routes/otherProfile.js
+++ social_bookmark/routes/otherProfile.js
@@ -22,6 +22,9 @@ router.get('/', (req, res) => {
         var thumbnailPath = result[0][0].image_path;
         var introduction = result[0][0].introduction;
         var userName = result[0][0].name;
+        if(introduction === null || introduction === ''){
+          introduction = '自己紹介';
+        }
         var values = {
           nickName,
           thumbnailPath,
@@ -33,6 +36,42 @@ router.get('/', (req, res) => {
     });
     return promise;
   })().then((values) => {
+    var promise = new Promise((resolve) => {
+      var selectBelongOrg = 'SELECT `org_id` FROM `organization_memberships` WHERE `user_id` = ?';
+      connection.query(selectBelongOrg, [target]).then((result) => {
+        values.selectedBelongOrgIds = result[0];
+        resolve(values);
+      });
+    });
+    return promise;
+  }).then((values) => {
+    var promise = new Promise((resolve) => {
+      var belongOrgIdsForQuery = '';
+      if(values.selectedBelongOrgIds.length > 0){
+        values.selectedBelongOrgIds.forEach((currentValue, index, array) => {
+          if(index + 1 === array.length){
+            belongOrgIdsForQuery += currentValue.org_id;
+            values.belongOrgIdsForQuery = belongOrgIdsForQuery;
+            resolve(values);
+          }else{
+            belongOrgIdsForQuery += currentValue.org_id + ' OR `id` = ';
+          }
+        });
+      }else{
+        resolve(values);
+      }
+    });
+    return promise;
+  }).then((values) => {
+    var promise = new Promise((resolve) => {
+      var selectOrgData = 'SELECT * FROM `organizations` WHERE `id` = ' + values.belongOrgIdsForQuery;
+      connection.query(selectOrgData).then((result) => {
+        values.orgData = result[0];
+        resolve(values);
+      });
+    });
+    return promise;
+  }).then((values) => {
     var selectRecentBookmarks = 'SELECT * FROM `bookmarks` WHERE `user_id` = ? ORDER BY `bookmark_id` DESC LIMIT 5';
     connection.query(selectRecentBookmarks, [target]).then((result) => {
       res.render('userProfile.ejs', {
@@ -41,6 +80,7 @@ router.get('/', (req, res) => {
         thumbnailPath : values.thumbnailPath,
         introduction : values.introduction,
         recentBookmarks : result[0],
+        orgData : values.orgData,
       });
     });
   });
