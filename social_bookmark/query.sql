56bab2dbaf807082a18acb758ab2d2e9e4e0143a
杉原 <shinta_16n1200325@nnn.ed.jp>
2016-09-07 14:01:06 +0900

    Enable to accept Emoji on DB

--- social_bookmark/query.sql
+++ social_bookmark/query.sql
@@ -1,13 +1,14 @@
 CREATE TABLE `users` (
   `user_id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(16) NOT NULL,
+  `name` varchar(16) NOT NULL DEFAULT '',
   `mail` varchar(255) NOT NULL,
-  `salt` char(8) NOT NULL,
-  `hash` char(64) NOT NULL,
-  `nick_name` varchar(32) DEFAULT NULL,
+  `salt` char(8) DEFAULT '',
+  `hash` char(64) DEFAULT '',
+  `nick_name` varchar(32) CHARACTER SET utf8mb4 DEFAULT NULL,
   `image_path` varchar(255) DEFAULT NULL,
-  `introduction` varchar(128) DEFAULT NULL,
+  `introduction` varchar(128) CHARACTER SET utf8mb4 DEFAULT NULL,
   PRIMARY KEY (`user_id`),
+  UNIQUE KEY `id` (`user_id`),
   UNIQUE KEY `name` (`name`),
   UNIQUE KEY `mail` (`mail`),
   UNIQUE KEY `user_id` (`user_id`)
@@ -15,8 +16,8 @@ CREATE TABLE `users` (
 
 CREATE TABLE `organizations` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(32) NOT NULL,
-  `introduction` varchar(128) DEFAULT NULL,
+  `name` varchar(32) CHARACTER SET utf8mb4 NOT NULL DEFAULT '',
+  `introduction` varchar(128) CHARACTER SET utf8mb4 DEFAULT NULL,
   `image_path` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id` (`id`)
@@ -32,20 +33,23 @@ CREATE TABLE `bookmarks` (
   `bookmark_id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) NOT NULL,
   `org_id` int(11) DEFAULT NULL,
-  `title` varchar(32) NOT NULL,
+  `title` varchar(32) CHARACTER SET utf8mb4 NOT NULL DEFAULT '',
   `url` varchar(2083) NOT NULL,
-  `description` varchar(128) DEFAULT NULL,
+  `description` varchar(128) CHARACTER SET utf8mb4 DEFAULT NULL,
+  `text` text CHARACTER SET utf8mb4,
   PRIMARY KEY (`bookmark_id`),
-  UNIQUE KEY `bookmark_id` (`bookmark_id`)
+  UNIQUE KEY `bookmark_id` (`bookmark_id`),
+  KEY `idx_bookmarks` (`text`(255))
 );
 
 CREATE TABLE `comments` (
   `comment_id` int(11) NOT NULL AUTO_INCREMENT,
   `bookmark_id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
-  `body` varchar(128) NOT NULL,
+  `body` varchar(128) CHARACTER SET utf8mb4 NOT NULL DEFAULT '',
   PRIMARY KEY (`comment_id`),
-  UNIQUE KEY `comment_id` (`comment_id`)
+  UNIQUE KEY `comment_id` (`comment_id`),
+  KEY `idx_comment` (`body`)
 );
 
 CREATE TABLE `sessions` (
